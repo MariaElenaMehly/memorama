014d10010423fe1329b7f148b70279e847cd7c13
MariaElenaMehly <26mariaelena12@gmail.com>
2020-01-20 14:11:13 -0600

    segunda parte de memorama

--- js/effects.js
+++ js/effects.js
@@ -3,7 +3,9 @@ var waitTimer='';
 var validateTimer=false;
 window.addEventListener('load', function(){
     var game=document.getElementById('game-box');
+    var stop=document.getElementById('stop');
     play.addEventListener('click', function(){
+        console.log('play')
         var memory=[];
         var i=0;
         var card='';
@@ -21,10 +23,10 @@ window.addEventListener('load', function(){
                 i++;
             }
         }while(i<12)
-        console.log(memory);
         document.getElementById('game-box').innerHTML=card;    
-        play.innerHTML="Iniciar juego";
         document.getElementById('timer').innerHTML="1:00";
+        play.classList.add('invisible');
+        buttons2.classList.remove('invisible');
         validateTimer=false;
         waitTimer=60;
         setTimeout(function(){
@@ -36,6 +38,7 @@ window.addEventListener('load', function(){
     
     game.addEventListener('click', function (evt) {
         var attribute=evt.target.getAttribute('class');
+        console.log(attribute);
         if (attribute!=null && attribute.indexOf('active') !== -1) {
             var id=evt.target.getAttribute('id');
             document.getElementById(id).classList.add('game-card');
@@ -71,6 +74,35 @@ window.addEventListener('load', function(){
             validateWinner();
         }
     });
+    stop.addEventListener('click', function(){
+        if(stop.innerHTML=='Detener')
+        {
+            validateTimer=false;
+            var active=document.querySelectorAll('.active');
+            for(var i=0; i<active.length; i++)
+            {
+                active[i].classList.remove('active');
+                active[i].classList.add('disabled');
+            }
+            stop.innerHTML='Continuar';
+        }else{
+            validateTimer=true;
+            var inactive=document.querySelectorAll('.disabled');
+            for(var i=0; i<inactive.length; i++)
+            {
+                inactive[i].classList.add('active');
+                inactive[i].classList.remove('disabled');
+            }
+            stop.innerHTML='Detener';  
+            timer();
+        }
+
+    });
+    restore.addEventListener('click', function(){
+        validateTimer=false;
+        waitTimer=0;
+        play.click();
+    });
 });
 
 function validateWinner()
@@ -80,22 +112,57 @@ function validateWinner()
     {
         var winner='<div class="winner"><img src="./../img/ganaste.gif" alt="Ganaste"></div>';
         document.getElementById('game-box').innerHTML=winner;
-        play.innerHTML="Jugar"; 
         if(validateTimer==true)
         {
             validateTimer=false;
         } 
+        buttons2.classList.add('invisible');
+        play.classList.remove('invisible');
+    }
+}
+function validateLoser()
+{
+    var timer=document.getElementById('timer').innerHTML;
+    var validate = document.querySelectorAll('.validate-true');
+    if(timer=='0:0' && validate.length<12)
+    {
+        validateTimer=false;
+        var active=document.querySelectorAll('.active');
+        for(var i=0; i<active.length; i++)
+        {
+            active[i].classList.remove('active');
+        }
+        document.getElementById('game-box').classList.add('invisible');
+        document.getElementById('buttons').classList.add('invisible');
+        document.getElementById('loser').classList.remove('invisible');
+        setTimeout(function(){
+            document.getElementById('game-box').classList.remove('invisible');
+            document.getElementById('buttons').classList.remove('invisible');     
+            document.getElementById('loser').classList.add('invisible');
+            buttons2.classList.add('invisible');
+            play.classList.remove('invisible');
+            var hidden=document.querySelectorAll('.game-card-hidden');
+            for(var i=0; i<active.length; i++)
+            {
+                hidden[i].classList.remove('game-card-hidden');
+                hidden[i].classList.add('game-card');
+            }
+        }, 2000)
     }
 }
 function timer()
 {
-    waitTimer=waitTimer-1;   
-    if(waitTimer>=0 && validateTimer==true)
+    if(validateTimer==true)
     {
-        document.getElementById('timer').innerHTML='0:'+waitTimer;
-        setTimeout('timer()', 1000);
-    }else{
-        validateTimer==false;
+        waitTimer=waitTimer-1;   
+        if(waitTimer>=0)
+        {
+            document.getElementById('timer').innerHTML='0:'+waitTimer;
+            setTimeout('timer()', 1000);
+        }else{
+            validateTimer==false;
+        }
+        validateLoser();
     }
     
 }
